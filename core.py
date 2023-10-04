62617c37751e0b57fbb3284ebd551b19a6a6158b
Svyatoslav <so.drozdov@gmail.com>
2023-10-04 13:40:24 +0300

    renamed variables

--- core.py
+++ core.py
@@ -7,15 +7,16 @@ def Solver(coefficients, initial_condition, my_function, butcher_matrix, x_max, 
     def f(position, y):
         return np.append(y[1:], my_function(position) - np.dot(coefficients, y[::-1]))
 
-    value_vector = np.array([initial_condition])
+    value_matrix = np.array([initial_condition])
     stages_number = len(A)
+    initial_cond_len = len(initial_condition)
     x_step = x_max / (split_number - 1)
     for n in range(0, split_number):
         coordinate = x_step * n
-        K = np.zeros((stages_number, len(initial_condition)))
-        K[0] = np.array([x_step * f(coordinate, value_vector[n])])
+        K = np.zeros((stages_number, initial_cond_len))
+        K[0] = np.array([x_step * f(coordinate, value_matrix[n])])
         for i in range(0, stages_number):
-            K[i] = x_step * f(coordinate + A[i] * x_step, value_vector[n] + np.dot(B[i], K))
-            value_vector[n] += G[i] * K[i]
-        value_vector = np.vstack((value_vector, value_vector[n]))
-    return value_vector
+            K[i] = x_step * f(coordinate + A[i] * x_step, value_matrix[n] + np.dot(B[i], K))
+            value_matrix[n] += G[i] * K[i]
+        value_matrix = np.vstack((value_matrix, value_matrix[n]))
+    return value_matrix
