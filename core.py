9ccd01e647ca3c023e2c3e5553a7909cc41c10fa
Svyatoslav <so.drozdov@gmail.com>
2023-10-04 13:50:20 +0300

    fix convergence

--- core.py
+++ core.py
@@ -15,8 +15,9 @@ def Solver(coefficients, initial_condition, my_function, butcher_matrix, x_max, 
         coordinate = x_step * n
         K = np.zeros((stages_number, initial_cond_len))
         K[0] = np.array([x_step * f(coordinate, value_matrix[n])])
+        delta_value_matrix = 0
         for i in range(0, stages_number):
             K[i] = x_step * f(coordinate + A[i] * x_step, value_matrix[n] + np.dot(B[i], K))
-            value_matrix[n] += G[i] * K[i]
-        value_matrix = np.vstack((value_matrix, value_matrix[n]))
+            delta_value_matrix += G[i] * K[i]
+        value_matrix = np.vstack((value_matrix, value_matrix[n]+delta_value_matrix))
     return value_matrix
